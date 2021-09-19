8116b714b9e399a442b28d47ec81563a7b747489
letuan <letuan2372000@gmail.com>
2021-09-19 12:12:45 +0700

    Buoc 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { taskName: '', tasks: [] };
+  }
+  myTaskChangeHandler = (event) => {
+    this.setState({ taskName: event.target.value });
+    // console.log('Hello', event.target.value) 
+  }
+  addTask=()=> {
+    // console.log('addTask', this.state.taskName)
+    this.setState({ taskName: ''});
+
+    this.state.tasks.push( this.state.taskName)
+
+  }
+  render() {
+    return (
+      <div className="App">
+          <br />
+          TODO LIST
+          <br />
+          <div className="aligned">
+            <input type="text" value={this.state.taskName} onChange={this.myTaskChangeHandler} />
+            <i className=" fas fa-plus-square" onClick={() => this.addTask()}></i>
+          </div>
+
+          <br />
+          <ul>
+            {this.state.tasks.map((taskItem,index) => (
+              <li key={index} className="taskItem">{taskItem}</li>
+            ))}
+          </ul>
+
+
+      </div>
+    )
+  }
 }
 
 export default App;
